ffc6bf6893b2160ec91bec2ef612239ca2eacffd
Eric Lai <jlai38@ucsc.edu>
2024-03-10 15:05:56 -0700

    15:01

--- YDOT.m
+++ YDOT.m
@@ -1,25 +1,31 @@
-function dydt = YDOT(t, y, m1, m2, L1, L2, gv)
-    % Function to compute the derivatives of the double pendulum system
-    
-    % Extract state variables
+function dydt = YDOT(t, y, m1, m2, L1, L2, g)
+    % Extract the state variables
     theta1 = y(1);
+    omega1 = y(2);
     theta2 = y(3);
-    theta1_dot = y(2);
-    theta2_dot = y(4);
+    omega2 = y(4);
+
+    % Compute derivatives
+    dydt = zeros(4, 1);
     
-    % Equations of motion for the double pendulum system
-    % Using Lagrangian Dynamics
-    A = (m1 + m2) * L1^2;
-    B = m2 * L1 * L2 * cos(theta1 - theta2);
-    C = m2 * L1 * L2;
-    D = m2 * L2^2;
-    E = -m2 * L1 * L2 * theta2_dot^2 * sin(theta1 - theta2) - (m1 + m2) * gv * L1 * sin(theta1);
-    F = m2 * L1 * L2 * theta1_dot^2 * sin(theta1 - theta2) - m2 * gv * L2 * sin(theta2);
-    
-    % Compute accelerations
-    theta1_ddot = (E*D - B*F) / (A*D - B*C);
-    theta2_ddot = (A*F - C*E) / (A*D - B*C);
-    
-    % Output derivatives
-    dydt = [theta1_dot; theta1_ddot; theta2_dot; theta2_ddot];
+    % Equation 1: d(theta1)/dt = omega1
+    dydt(1) = omega1;
+
+    % Equation 2: d(theta2)/dt = omega2
+    dydt(3) = omega2;
+
+    % Intermediate terms
+    d = theta1 - theta2;
+    c = cos(d);
+    s = sin(d);
+
+    % Equations 3 and 4: Compute angular accelerations
+    num1 = -g * (2*m1 + m2) * sin(theta1) - m2 * g * sin(theta1 - 2*theta2);
+    num2 = -2 * sin(theta1 - theta2) * m2 * (omega2^2 * L2 + omega1^2 * L1 * cos(theta1 - theta2));
+    den1 = L1 * (2 * m1 + m2 - m2 * cos(2 * (theta1 - theta2)));
+    dydt(2) = (num1 + num2) / den1;
+
+    num3 = 2 * sin(theta1 - theta2) * (omega1^2 * L1 * (m1 + m2) + g * (m1 + m2) * cos(theta1) + omega2^2 * L2 * m2 * cos(theta1 - theta2));
+    den2 = L2 * (2 * m1 + m2 - m2 * cos(2 * (theta1 - theta2)));
+    dydt(4) = num3 / den2;
 end
